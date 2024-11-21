eff948b6c7af1fb5aae02756aa1412496e42eea0
liuzhe <liuzhe@qq.com>
2024-11-21 23:53:22 +0800

    feat:增加webpack模板

--- bin/config.js
+++ bin/config.js
@@ -1,5 +1,6 @@
 const projectList = {
-    vueRouter: "https://github.com/ConsoleLZ/vue-router-cli.git"
+    vueRouter: "https://github.com/ConsoleLZ/vue-router-cli.git",
+    webpackMPA: "https://github.com/ConsoleLZ/webpack-mpa-template.git"
 };
 
 module.exports = projectList;
