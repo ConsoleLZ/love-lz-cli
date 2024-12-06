ca92edb637d7bdd975aa4238ab8a6d8af373b00f
liuzhe <liuzhe@qq.com>
2024-12-06 23:59:13 +0800

    feat:add

--- bin/config.js
+++ bin/config.js
@@ -1,6 +1,7 @@
 const projectList = {
     vueRouter: "https://github.com/ConsoleLZ/vue-router-cli.git",
-    ssgTemplate: "https://github.com/ConsoleLZ/ssg-template.git"
+    ssgTemplate: "https://github.com/ConsoleLZ/ssg-template.git",
+    vueTsTemplate: "https://github.com/ConsoleLZ/vue-ts-template.git",
 };
 
 module.exports = projectList;
