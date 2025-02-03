be35892465933ec7ecfd290b66a74369e93d5c6a
liuzhe <1804319025@qq.com>
2025-02-04 01:21:47 +0800

    feat:reactTs

--- bin/config.js
+++ bin/config.js
@@ -2,6 +2,7 @@ const projectList = {
     vueRouter: "https://github.com/ConsoleLZ/vue-router-cli.git",
     ssgTemplate: "https://github.com/ConsoleLZ/ssg-template.git",
     vueTsTemplate: "https://github.com/ConsoleLZ/vue-ts-template.git",
+    reactTsTemplate: "https://github.com/ConsoleLZ/react-ts-temp.git",
 };
 
 module.exports = projectList;
