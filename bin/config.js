7f08640f98ae7dc972b76655bd80d3117bb0afef
liuzhe <1804319025@qq.com>
2025-07-09 10:39:59 +0800

    feat

--- bin/config.js
+++ bin/config.js
@@ -5,7 +5,8 @@ const projectList = {
     reactTsTemplate: "https://github.com/ConsoleLZ/react-ts-temp.git",
     reactRouterTsTemplate: "https://github.com/ConsoleLZ/react-router-ts-temp.git",
     vercelNode: "https://github.com/ConsoleLZ/vercel-node.git",
-    platformioSTM32: "https://github.com/ConsoleLZ/platformioSTM32.git"
+    platformioSTM32: "https://github.com/ConsoleLZ/platformioSTM32.git",
+    qtWinCustom: "https://github.com/ConsoleLZ/qt-win-custom-temp.git"
 };
 
 module.exports = projectList;
