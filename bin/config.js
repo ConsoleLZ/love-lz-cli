df13de13896da1ac369addd2db0a9295d2f6c0dc
liuzhe <1804319025@qq.com>
2025-08-21 19:09:04 +0800

    feat

--- bin/config.js
+++ bin/config.js
@@ -1,6 +1,6 @@
 const codeUrl = 'https://github.com/ConsoleLZ/code-template.git'
 
-const projectList = ['vue-single', 'vue-router', 'react-single', 'react-router', 'node-express', 'vercel-node', 'ssg', 'platformioSTM32', 'QT-win']
+const projectList = ['vue-single', 'vue-router', 'react-single', 'react-router', 'node-express', 'vercel-node', 'ssg', 'platformioSTM32', 'QT-win', 'electron-vue-vite']
 
 module.exports = {
     codeUrl,
