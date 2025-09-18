442e7e6369451becf9976b5e36f609fb8bcbd986
liuzhe <1804319025@qq.com>
2025-09-18 14:04:56 +0800

    feat

--- bin/config.js
+++ bin/config.js
@@ -1,8 +1,21 @@
-const codeUrl = 'https://github.com/ConsoleLZ/code-template.git'
+const codeUrl = "https://github.com/ConsoleLZ/code-template.git";
 
-const projectList = ['vue-single', 'vue-router', 'react-single', 'react-router', 'node-express', 'vercel-node', 'ssg', 'platformioSTM32', 'QT-win', 'electron-vue-vite', 'electron-vue-vite-sass']
+const projectList = [
+  "vue-single",
+  "vue-router",
+  "react-single",
+  "react-router",
+  "node-express",
+  "vercel-node",
+  "ssg",
+  "platformioSTM32",
+  "QT-win",
+  "electron-vue-vite",
+  "electron-vue-vite-sass",
+  "react-native"
+];
 
 module.exports = {
-    codeUrl,
-    projectList
-}
+  codeUrl,
+  projectList,
+};
